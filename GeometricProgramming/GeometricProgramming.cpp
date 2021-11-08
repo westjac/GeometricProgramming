3552993d58579dc0f39207867f1fa942aa678c41
Jacob West <jwest@omnitech-inc.com>
2021-11-08 16:49:12 -0700

    Reading in polygons from file

--- GeometricProgramming/GeometricProgramming.cpp
+++ GeometricProgramming/GeometricProgramming.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <sstream>
 #include "Line.h"
 
 using namespace std;
@@ -16,6 +17,11 @@ int main(int argc, char *argv[])
         string fileName = argv[1];
         inFile.open(fileName);
     }
+    else
+    {
+        cout << "Wrong number of arguments" << endl;
+        return -1;
+    }
 
     if (!inFile.is_open())
         {
@@ -27,10 +33,40 @@ int main(int argc, char *argv[])
     string line = "";
     vector<Line> lines;
     
+    //Gets a line for each polygon
     while(getline(inFile, line))
-        cout << line << endl;
-        //stringstream ss(line)
+    {    
+        int polygonId = 0;
+        vector<Point> points;
+        stringstream ss(line);
+        int x = 0;
+        int y = 0;
+        points.clear();
+        //Get all points for the given line
+        while(ss >> x >> y)
+        {
+            points.push_back(Point(x, y));
+        }
+    
+        //create line segments
+        for(int i = 0; i < points.size(); i++)
+        {
+            if( i == points.size() - 1) //Special case for last item
+            {
+                lines.push_back(Line(points[i], points[0], polygonId));
+            }
+            else
+            {
+                lines.push_back(Line(points[i], points[i+1], polygonId));
+            }
+        }
+    }
+    
+    //At this point I now have a vector of lines read in from file,
+    //    where each line has a polygon id.
 
-    Point point = Point(1, 1);
+
+
+
 }
 
